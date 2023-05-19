554c06e76bebc2df5615339eaf1d2a0d631e1924
prakhar chandra gupta <prakharguptahr10@gmail.com>
2023-05-20 01:28:55 +0530

    special-symbol-removed

--- src/pages/about.js
+++ src/pages/about.js
@@ -71,20 +71,20 @@ const about = () => {
             <div className='col-span-3 flex flex-col items-start justify-start '>
                 <h2 className='mb-4  test-lg font-bold uppercase text-dark/75 dark:text-light/75 '>BIOGRAPHY</h2>
                 <p className='font-medium '>
-                Hi, I'm Prakhar Chandra Gupta, a Full Stack  web developer in MERN Stack 
+                Hi, I&apos;m Prakhar Chandra Gupta, a Full Stack  web developer in MERN Stack 
                 and UI/UX designer with a passion for creating beautiful, functional, 
                 and user-centered digital experiences. With 06 months of experience in the field. I 
-                am always looking for new and innovative ways to bring my clients' 
+                am always looking for new and innovative ways to bring my clients&apos;
                 visions to life.
                 </p>
 
                 <p className=' my-4 font-medium '>
                    I believe that design & development is about more than just making things look pretty – 
-                   it's about solving problems and creating intuitive, enjoyable experiences for users.
+                   it&apos;s about solving problems and creating intuitive, enjoyable experiences for users.
                 </p>
 
                 <p className='font-medium '>
-                 Whether I'm working on a website, mobile app, or other digital product, I bring my commitment 
+                 Whether I&apos;m working on a website, mobile app, or other digital product, I bring my commitment 
                  to design excellence and user-centered thinking to every project I work on. I look forward to 
                  the opportunity to bring my skills and passion to your next project.
                 </p>
